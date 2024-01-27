1ca5d87f859f11745bed9e2955760c998dc9ae03
Thomas Reichert <thomasr1987@gmail.com>
2024-01-28 06:53:44 +1100

    Delete underused test code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,6 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
 
 tasks.register<Exec>("compileRust") {
