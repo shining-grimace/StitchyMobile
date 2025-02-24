8f5d5be7c86b9237347023e5e7832b89dd7b43b3
Thomas Reichert <thomasr1987@gmail.com>
2025-02-25 06:28:49 +1100

    Bumped NDK and confirmed running on Android 15 with 16 KB page size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0-alpha10")
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
