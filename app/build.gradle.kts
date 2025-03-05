b7fe295a315810718b72bf047b19dc317b84bbb0
Thomas Reichert <thomasr1987@gmail.com>
2025-03-06 05:55:50 +1100

    Bump AGP and other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,9 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("com.google.android.material:material:1.13.0-alpha10")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.7")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.7")
+    implementation("com.google.android.material:material:1.13.0-alpha11")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.8")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.8")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
     implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("io.coil-kt.coil3:coil:3.1.0")
